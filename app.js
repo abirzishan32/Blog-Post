dedd7323742813da798b755124ec24dcf4f8b870
abir_zishan <rahman2007026@stud.kuet.ac.bd>
2024-08-15 20:54:46 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const connectDB = require('./server/config/db');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+//Connecting to Database
+connectDB();
+
+
+app.use(express.static('public'));
+
+//Template Layout
+app.use(expressLayouts);
+app.set('layout', './layouts/main');
+app.set('view engine', 'ejs');
+
+
+app.use('/', require('./server/routes/main'));
+
+app.post("/register", (req, res) => {
+    
+});
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
