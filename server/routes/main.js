4b455485fb3848c22f77a564d044c46f444a00e8
abir_zishan <rahman2007026@stud.kuet.ac.bd>
2024-08-16 02:50:58 +0600

    dummy post uploaded in db

--- server/routes/main.js
+++ server/routes/main.js
@@ -1,9 +1,33 @@
 const express = require('express');
 const router = express.Router();
+const Post = require('../models/posts');
+
 
 router.get('', (req, res) => {
     res.render('index');
     }
 );
+
+
+
+function insertPostData() {
+    Post.insertMany([
+        {
+            title: "Building APIs with Node.js",
+            content: "Learn how to use Node.js to build RESTful APIs using frameworks like Express.js"
+        }
+    ])
+    .then(() => {
+        console.log("Data inserted successfully");
+    })
+    .catch(err => {
+        console.error("Error inserting data:", err);
+    });
+}
+
+
+insertPostData();
+
+
 
 module.exports = router;
