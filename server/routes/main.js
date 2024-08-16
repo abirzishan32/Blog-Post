364039adbef07eb6bbab4301df2b853c1f8a1b5f
abir_zishan <rahman2007026@stud.kuet.ac.bd>
2024-08-16 20:59:23 +0600

    pagination added

--- server/routes/main.js
+++ server/routes/main.js
@@ -3,31 +3,44 @@ const router = express.Router();
 const Post = require('../models/posts');
 
 
-router.get('', (req, res) => {
-    res.render('index');
-    }
-);
+router.get('', async (req, res) => {  
+    try {
 
-
-
-function insertPostData() {
-    Post.insertMany([
-        {
-            title: "Building APIs with Node.js",
-            content: "Learn how to use Node.js to build RESTful APIs using frameworks like Express.js"
+        const locals = {
+            title: 'Blog Post',
+            description: "A blog website created using Node.js, Express, and MongoDB."
         }
-    ])
-    .then(() => {
-        console.log("Data inserted successfully");
-    })
-    .catch(err => {
-        console.error("Error inserting data:", err);
-    });
-}
 
 
-insertPostData();
+        //Pagination
+        let perPage = 10; // Number of posts per page
+        let page = req.query.page || 1; // Query string for page number, if not provided, default to 1
 
 
+
+        const data = await Post.aggregate([{$sort : {createdAt : -1}}]) // Sort by date in descending order
+        .skip(perPage * page - perPage) // Skip the first n posts
+        .limit(perPage) // Limit the number of posts to be displayed
+        .exec(); // Execute the query
+
+
+        //Counting total number of posts
+        const count = await Post.countDocuments();
+        const nextPage = parseInt(page) + 1;
+        const hasNextPage = nextPage <= Math.ceil(count / perPage);
+
+
+
+        res.render('index', { 
+            locals,
+             data,
+             current : page, // Current page number
+             nextPage : hasNextPage ? nextPage : null // Next page number
+             });
+    } 
+    catch(err) {
+        console.log(err);
+    }
+});
 
 module.exports = router;
