2d10eaac786dd0a78c3cfd0e04278e70674ff37c
abir_zishan <rahman2007026@stud.kuet.ac.bd>
2024-08-17 16:13:39 +0600

    single post view feature added

--- server/routes/main.js
+++ server/routes/main.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const Post = require('../models/posts');
 
 
+
 router.get('', async (req, res) => {  
     try {
 
@@ -19,16 +20,19 @@ router.get('', async (req, res) => {
 
 
         const data = await Post.aggregate([{$sort : {createdAt : -1}}]) // Sort by date in descending order
-        .skip(perPage * page - perPage) // Skip the first n posts
-        .limit(perPage) // Limit the number of posts to be displayed
+        .skip(perPage * page - perPage)     /*  This calculation determines how many posts to skip, based on the current page.
+                                            For example, if you're on page 2, it will skip the first 10 posts (10 * 2 - 10 = 10),
+                                            so you start from the 11th post. */
+        .limit(perPage) // This limits the number of posts retrieved to the perPage value, ensuring only 10 posts are fetched for the current page.
         .exec(); // Execute the query
 
 
         //Counting total number of posts
-        const count = await Post.countDocuments();
-        const nextPage = parseInt(page) + 1;
-        const hasNextPage = nextPage <= Math.ceil(count / perPage);
-
+        const count = await Post.countDocuments(); // Count the total number of posts
+        const nextPage = parseInt(page) + 1; // Next page number
+        const hasNextPage = nextPage <= Math.ceil(count / perPage); /*Math.ceil(count / perPage) calculates the total number of pages needed. 
+                                                                    If nextPage is less than or equal to this total, it means there is another page available,
+                                                                     and hasNextPage will be true.  */
 
 
         res.render('index', { 
@@ -42,5 +46,40 @@ router.get('', async (req, res) => {
         console.log(err);
     }
 });
+
+
+
+
+/* setting up a route that handles requests for individual blog posts. */
+router.get('/post/:id', async (req, res) => {  /* a route that handles GET requests to URLs that match the pattern /post/:id.
+                                                :id is a route parameter (often called a "slug" in the context of blog posts) that
+                                                 acts as a placeholder for the actual ID of a blog post. */
+    try {
+      let slug = req.params.id;    /* retrieves the value of the id parameter from the URL.
+                                     This value is stored in the variable slug, which represents the unique identifier
+                                      for the blog post you want to retrieve. */
+  
+      const data = await Post.findById({ _id: slug });  /* uses Mongoose to query the MongoDB database and retrieve the blog post
+                                                         with the specific _id that matches the slug. */
+  
+      const locals = {
+        title: data.title,
+        description: "Simple Blog created with NodeJs, Express & MongoDb.",
+      }
+  
+      res.render('post', { 
+        locals,
+        data,
+        currentRoute: `/post/${slug}`  /* The current URL path (/post/${slug}), which might be used for 
+                                    navigation or highlighting the active route in the UI. */
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  
+  });
+
+
+
 
 module.exports = router;
